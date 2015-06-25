27d15b455fe57e5a226b198c8839043e40b3eca6
ashic <ashic@live.com>
2015-06-25 01:11:27 +0100

    exercise 1 done

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -1,8 +1,20 @@
+import org.apache.spark.SparkContext
+
 /**
  * Created by Ashic on 24/06/2015.
  */
+
+
 object Main {
   def main(args: Array[String]){
+    val sc = new SparkContext("local[*]", "hello-spark")
 
+    val rdd = sc.parallelize(1 to 10)
+
+    val even = rdd.filter(_ % 2 == 0)
+
+    even.foreach(println)
+
+    sc.stop()
   }
 }
